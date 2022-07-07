3f49f14125ffc3a50e35005b2cd34fa328193bba
Sanjar1218 <71938677+Sanjar1218@users.noreply.github.com>
2022-07-07 11:40:36 +0500

    Update basic_math_function04.dart

--- bin/basic_math_function04.dart
+++ bin/basic_math_function04.dart
@@ -2,7 +2,7 @@
     Create function named func with a argument
     Multiply one number by the sine of another number.
     Args:
-        y  (int): radian number
+        y  (double): radian number
         x (int): multiply x by the sine of y
     Returns:
         result (double): multiply x by the sine of y. Round the result.
